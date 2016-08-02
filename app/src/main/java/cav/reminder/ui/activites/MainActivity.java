66891bf4d620ba30f8c9149c9cd786824fe714d1
CavInc <cavinc20@gmail.com>
2016-08-02 07:36:51 +0700

    Добавлены прототипы

---
+++ app/src/main/java/cav/reminder/ui/activites/MainActivity.java
@@ -0,0 +1,15 @@
+package cav.reminder.ui.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cav.reminder.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
