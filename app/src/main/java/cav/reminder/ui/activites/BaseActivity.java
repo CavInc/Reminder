4be71b1aa53451e476716de47207922417d6b8ea
CavInc <cavinc20@gmail.com>
2016-08-02 22:04:14 +0700

    Исправлены косяки в темах добавлен файл стилей и доплнительные классы

---
+++ app/src/main/java/cav/reminder/ui/activites/BaseActivity.java
@@ -0,0 +1,51 @@
+package cav.reminder.ui.activites;
+
+
+import android.app.ProgressDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import cav.reminder.R;
+import cav.reminder.utils.ConstantManager;
+
+public class BaseActivity extends AppCompatActivity{
+    private final String TAG = ConstantManager.TAG_PREFIX;
+
+    protected ProgressDialog mProgressDialog;
+
+    public void showProgress(){
+/*
+        if (mProgressDialog==null) {
+            mProgressDialog = new ProgressDialog(this,R.style.custom_dialog);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            mProgressDialog.show();
+            mProgressDialog.setContentView(R.layout.progress_splash);
+        }else{
+            mProgressDialog.show();
+            mProgressDialog.setContentView(R.layout.progress_splash);
+        }
+*/
+    }
+    public void hideProgress(){
+        if (mProgressDialog!=null){
+            if (mProgressDialog.isShowing()){
+                mProgressDialog.hide();
+            }
+        }
+    }
+
+    // показываем сообщение о ошибке
+    public void showError(String message,Exception error){
+        showToast(message);
+        Log.e(TAG,String.valueOf(error));
+    }
+
+    // показываем высплывающее сообщение
+    public void showToast(String message){
+        Toast.makeText(this,message,Toast.LENGTH_LONG).show();
+    }
+}
---
+++ app/src/main/java/cav/reminder/utils/ConstantManager.java
@@ -0,0 +1,8 @@
+package cav.reminder.utils;
+
+/**
+ * Created by cav on 02.08.16.
+ */
+public interface ConstantManager {
+    String TAG_PREFIX = "REMINDER";
+}
