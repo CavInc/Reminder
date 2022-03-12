d5887d4f077e0dcc6083120e91a119c6f7ddda68
CavInc <cavinc20@gmail.com>
2022-03-12 21:51:28 +0700

    Работа над масштабироваем софранненого фото

---
+++ app/src/main/java/cav/reminder/ui/activites/AboutActivity.java
@@ -0,0 +1,16 @@
+package cav.reminder.ui.activites;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by cav on 12.03.22.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/cav/reminder/ui/activites/PhotoViewActivity.java
+++ app/src/main/java/cav/reminder/ui/activites/PhotoViewActivity.java
@@ -2,6 +2,8 @@ package cav.reminder.ui.activites;
 
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.widget.ImageView;
 
 import java.io.File;
@@ -16,12 +18,16 @@ public class PhotoViewActivity extends AppCompatActivity {
     private ImageView mPhotoView;
     private File mPhotoFile;
 
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScaleFactor = 1.0f;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_view);
 
         mPhotoView = (ImageView) findViewById(R.id.photo_view_iv);
+        mScaleGestureDetector = new ScaleGestureDetector(this, new ScaleListener());
 
         String sf = getIntent().getStringExtra(ConstantManager.RECORD_PHOTO_FILE);
         if (sf != null) {
@@ -37,5 +43,20 @@ public class PhotoViewActivity extends AppCompatActivity {
             mPhotoView.setImageBitmap(Func.getPicSize(mPhotoFile.toString()));
         }
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return mScaleGestureDetector.onTouchEvent(event);
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScaleFactor *= detector.getScaleFactor();
+            mPhotoView.setScaleX(mScaleFactor);
+            mPhotoView.setScaleY(mScaleFactor);
+            return true;
+        }
     }
 }
