c82a8f1880a8b360822c25d6e3ef40e72dd42492
CavInc <cavinc20@gmail.com>
2022-03-19 10:50:21 +0700

    Добавлен логотип и немного изменена разметка

--- app/src/main/java/cav/reminder/ui/activites/AboutActivity.java
+++ app/src/main/java/cav/reminder/ui/activites/AboutActivity.java
@@ -1,9 +1,12 @@
 package cav.reminder.ui.activites;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import cav.reminder.BuildConfig;
 import cav.reminder.R;
 
@@ -16,7 +19,27 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setupToolbar(toolbar);
 
         ((TextView) findViewById(R.id.about_vesion)) .setText(BuildConfig.VERSION_NAME);
+    }
+
+
+    private void setupToolbar(Toolbar toolbar) {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return true;
     }
 }
